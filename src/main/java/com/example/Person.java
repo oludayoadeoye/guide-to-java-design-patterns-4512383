5889f77bdd4db45b27cbd1f0f51a193d81fc4aeb
oludayoadeoye <oludayoadeoye99@gmail.com>
2024-07-22 12:06:00 -0400

    hi

--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -12,14 +12,13 @@ public class Person
     private final String emailAddress;
     private final String phoneNumber;
 
-    public Person(String firstName, String middleName, String lastName,
-                  LocalDate dateOfBirth, String emailAddress, String phoneNumber) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.emailAddress = emailAddress;
-        this.phoneNumber = phoneNumber;
+    private Person(PersonBuilder personBuilder ){
+        this.firstName = personBuilder.firstName;
+        this.middleName = personBuilder.middleName;
+        this.lastName = personBuilder.lastName;
+        this.dateOfBirth = personBuilder.dateOfBirth;
+        this.emailAddress = personBuilder.emailAddress;
+        this.phoneNumber = personBuilder.phoneNumber;
         if (firstName == null || lastName == null) {
             throw new IllegalArgumentException("First name and last name must not be null");
         }
@@ -38,7 +37,6 @@ public class Person
     }
 
     public static class PersonBuilder {
-
         private final String firstName;
         private String middleName;
         private final String lastName;
@@ -51,32 +49,31 @@ public class Person
             this.lastName = lastName;
         }
 
-        public PersonBuilder middleName(String middleName) {
+        public  PersonBuilder middleName(String middleName) {
             this.middleName = middleName;
             return this;
         }
 
-        public PersonBuilder dateOfBirth(LocalDate dateOfBirth) {
+        public  PersonBuilder dateOfBirth(LocalDate dateOfBirth) {
             this.dateOfBirth = dateOfBirth;
             return this;
         }
 
-        public PersonBuilder emailAddress(String emailAddress) {
+        public  PersonBuilder emailAddress(String emailAddress) {
             this.emailAddress = emailAddress;
             return this;
         }
 
-        public PersonBuilder phoneNumber(String phoneNumber) {
+        public  PersonBuilder phoneNumber(String phoneNumber) {
             this.phoneNumber = phoneNumber;
             return this;
         }
 
+
+
         public Person build() {
-            return new Person(firstName, middleName, lastName,
-                    dateOfBirth, emailAddress, phoneNumber);
+            return new Person(this);
         }
-
-
     }
 
 }
--- src/main/java/com/example/SalesLeadTracker.java
+++ src/main/java/com/example/SalesLeadTracker.java
@@ -6,17 +6,26 @@ public class SalesLeadTracker {
 
     public static void main(String[] args) {
 
-        var person1 = new Person.PersonBuilder("Tracy", "Westbay")
-                .dateOfBirth(LocalDate.of(1985, 1, 1))
+        var person1 =new Person.PersonBuilder("Tracy", "Westbay")
+                .dateOfBirth(LocalDate.of(1985,1,1))
                 .emailAddress("tracy@example.com")
                 .build();
 
+
+//                new Person("Tracy", null,
+//                "Westbay", LocalDate.of(1985,1,1),
+//                "tracy@example.com",null);
+
         addPersonToLeadTracker(person1);
 
-        var person2 = new Person.PersonBuilder("Jerome", "Donaldson")
-                .middleName("Henry")
+        var person2 = new Person.PersonBuilder("Jerome", "Henry")
+                .middleName("Donaldson")
                 .phoneNumber("123456789")
                 .build();
+
+//                new Person("Jerome", "Henry",
+//                "Donaldson", null,
+//                null, "123456789");
 
         addPersonToLeadTracker(person2);
     }
